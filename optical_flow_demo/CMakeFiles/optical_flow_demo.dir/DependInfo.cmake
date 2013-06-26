d72ba7d0b75771097a86135fdca243d2a6bb1304
jcvazquez <jcvazquez.juan.vazquez006@mymdc.net>
2013-06-26 14:27:40 -0700

    clean up & optical flow example

---
+++ optical_flow_demo/CMakeFiles/optical_flow_demo.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/trafficvision/Desktop/optical_flow_demo/optical_flow_demo.cpp" "/home/trafficvision/Desktop/optical_flow_demo/CMakeFiles/optical_flow_demo.dir/optical_flow_demo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ optical_flow_demo/CMakeFiles/optical_flow_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/optical_flow_demo.dir/optical_flow_demo.cpp.o"
+  "optical_flow_demo.pdb"
+  "optical_flow_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/optical_flow_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
