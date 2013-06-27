c538ab7a31330c967892e919377a6f0045795fed
jcvazquez <jcvazquez.juan.vazquez006@mymdc.net>
2013-06-27 13:47:58 -0700

    Added the testTV program with optical flow demo

---
+++ testTrafficVision/CMakeFiles/testTrafficVision.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testTrafficVision.dir/testTrafficVision.cpp.o"
+  "testTrafficVision.pdb"
+  "testTrafficVision"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testTrafficVision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
