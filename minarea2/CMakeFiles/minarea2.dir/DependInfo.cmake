90138b006369c442f9f29eabb703592b42439599
jcvazquez <jcvazquez.juan.vazquez006@mymdc.net>
2013-07-10 13:47:36 -0700

    bounding box 2D

---
+++ minarea2/CMakeFiles/minarea2.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/trafficvision/Desktop/minarea2/minarea2.cpp" "/home/trafficvision/Desktop/minarea2/CMakeFiles/minarea2.dir/minarea2.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ minarea2/CMakeFiles/minarea2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/minarea2.dir/minarea2.cpp.o"
+  "minarea2.pdb"
+  "minarea2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/minarea2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
